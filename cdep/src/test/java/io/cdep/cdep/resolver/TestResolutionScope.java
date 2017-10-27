78705a167acb160192177a69f9abd7a906db2238
Jomo Fisher <jomof@google.com>
2017-10-27 12:15:45 -0700

    Fix toposort of dependencies

--- cdep/src/test/java/io/cdep/cdep/resolver/TestResolutionScope.java
+++ cdep/src/test/java/io/cdep/cdep/resolver/TestResolutionScope.java
@@ -165,7 +165,7 @@ public class TestResolutionScope {
     CDepManifestYml appManifest = new CDepManifestYml(CoordinateUtils.tryParse("com.github.jomof:firebase/app:2.1.3-rev7"));
     SoftNameDependency appUnresolved = scope.getUnresolvedReferences().iterator().next();
     ResolvedManifest appResolved = new ResolvedManifest(new URL("http://www.google.com"), appManifest);
-    scope.recordResolved(appUnresolved, appResolved, new ArrayList<>());
+    scope.recordResolved(appUnresolved, appResolved, new ArrayList<HardNameDependency>());
     assertThat(scope.isResolutionComplete()).isTrue();
     assertThat(scope.getResolutions()).hasSize(2);
   }
