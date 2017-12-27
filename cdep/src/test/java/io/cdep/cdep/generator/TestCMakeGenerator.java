8a79aa909f5bfcccd91e2db19cec0868247b19fd
Steven Winston <gpx1000@gmail.com>
2017-12-26 16:38:34 -0800

    This shouldn't turn it green but switch back to PRIVATE to see if Re2 requires private scope and not interface (if it does fix then will try Public instead).

--- cdep/src/test/java/io/cdep/cdep/generator/TestCMakeGenerator.java
+++ cdep/src/test/java/io/cdep/cdep/generator/TestCMakeGenerator.java
@@ -78,7 +78,7 @@ public class TestCMakeGenerator {
     FunctionTableExpression table = builder.build();
     String result = new CMakeGenerator(environment, table).create();
     System.out.printf(result);
-    boolean headerOnly = result.contains("target_compile_features(${target} INTERFACE cxx_auto_type cxx_decltype)");
+    boolean headerOnly = result.contains("target_compile_features(${target} PRIVATE cxx_auto_type cxx_decltype)");
     boolean archive = result.contains("INTERFACE_COMPILE_FEATURES cxx_auto_type cxx_decltype");
     assert(headerOnly || archive);
   }
