accb1918644febd0bff1834c5e33dd63d5765f78
Jomo Fisher <jomofisher@gmail.com>
2018-05-10 17:42:35 -0700

    More

--- boringssl/build-boringssl.sh
+++ boringssl/build-boringssl.sh
@@ -3,8 +3,9 @@ BORINGSSL_STAMP=51776b0aebcd658c0f8867eaf5bce5ac80afb4df
 curl -L -o boringssl.zip https://github.com/google/boringssl/archive/${BORINGSSL_STAMP}.zip
 unzip boringssl.zip > boringssl-unzip.log
 
+echo cd boringssl-${BORINGSSL_STAMP}
 cd boringssl-${BORINGSSL_STAMP}
-cp ../cmakeify.ssl.yml cmakeify.yml
+cp ../cmakeify-ssl.yml cmakeify.yml
 ../../cmakeify --group-id com.github.google --artifact-id boringssl/ssl --target-version 0.0.0
-cp ../cmakeify.crypto.yml cmakeify.yml
+cp ../cmakeify-crypto.yml cmakeify.yml
 ../../cmakeify --group-id com.github.google --artifact-id boringssl/crypto --target-version 0.0.0
